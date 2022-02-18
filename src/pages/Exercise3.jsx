a24212034c6c6477db39ed7b7909ac8a94a78c65
davidnherran <davidnherran@gmail.com>
2022-02-18 06:58:50 -0500

    :zap: Solved: Figma exercise 3

--- src/pages/Exercise3.jsx
+++ src/pages/Exercise3.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { gql, useQuery } from '@apollo/client'
+import starFull from '../assets/images/Star3.png'
+import starEmpty from '../assets/images/Star5.png'
+import search from '../assets/images/Search.png'
+import filter from '../assets/images/FilterIcon.png'
+import sort from '../assets/images/ArrowIcon.png'
 
 const ALL_MOVIES = gql`
 query {
@@ -62,9 +67,9 @@ const Exercise3 = () => {
     const stars = []
     for (let i = 0; i < 5; i++) {
       if (i < voteAverage / 2) {
-        stars.push('S')
+        stars.push(<img key={i} src={starFull} alt='star' />)
       } else {
-        stars.push('N')
+        stars.push(<img key={i} src={starEmpty} alt='star' />)
       }
     }
     return stars
@@ -94,42 +99,49 @@ const Exercise3 = () => {
   }
 
   return (
-    <>
+    <div className='movies'>
       <Navbar />
       <div className='container'>
         <h1>Películas</h1>
-        <input type='text' onChange={handleSearch} /> <span onClick={() => { filterMoviesByGenrer(18, 12) }}>Ordenar</span>
-
-        <div className='grid'>
-          {loading
-            ? <p>Loading...</p>
-            : listMovies.length > 0
-              ? listMovies.map((movie, i) => (
-                <div className='card' key={i}>
-                  <h2>{`${movie.title} (${movie.release_date.split('-')[0]})`}</h2>
-                  <div className='card__body'>
-                    <div className='card__body-img'>
-                      <img width={180} height={270} src={`${imguri}${movie.poster_path}`} alt='' />
-                    </div>
-                    <div className='card__body-overview'>
-                      <p>{`${movie.overview.substr(0, 365)}...`}</p>
-                      <div>Titulo: {movie.title}</div>
-                      <div>Calificacion: {movie.vote_average} {getStars(Math.floor(movie.vote_average))}</div>
-                      <div>Genero:  {getGenre(genrer, movies[i].genre_ids)}</div>
-                      <div>Fecha de realización: {movie.release_date}</div>
+        <input className='movies__search' type='text' onChange={handleSearch} />
+        <img className='movies__search-icon' src={search} />
+        <div className='movies__filter'>
+          <img className='movies__filter-icon' src={filter} alt='' />
+        </div>
+        <div className='movies__sort' onClick={() => { filterMoviesByGenrer(18, 12) }}>
+          Ordenar <img className='movies__sort-icon' src={sort} />
+        </div>
+        <div className='movies__scrollable'>
+          <div className='grid'>
+            {loading
+              ? <p>Loading...</p>
+              : listMovies.length > 0
+                ? listMovies.map((movie, i) => (
+                  <div className='card' key={i}>
+                    <h2>{`${movie.title} (${movie.release_date.split('-')[0]})`}</h2>
+                    <div className='card__body'>
+                      <div className='card__body-img'>
+                        <img width={180} height={270} src={`${imguri}${movie.poster_path}`} alt='' />
+                      </div>
+                      <div className='card__body-overview'>
+                        <p>{`${movie.overview.substr(0, 365)}...`}</p>
+                        <div className='card__body-description'>
+                          <div><span>Titulo: </span> {movie.title}</div>
+                          <div><span>Calificacion: </span> {movie.vote_average}
+                            {' '}{getStars(Math.floor(movie.vote_average))}
+                          </div>
+                          <div><span>Genero: </span> {getGenre(genrer, movies[i].genre_ids)}</div>
+                          <div><span>Fecha de realización: </span> {movie.release_date}</div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-                ))
-              : <p>Ning&uacute;n resultado coincide con tu b&uacute;squeda</p>}
+                  ))
+                : <p>Ning&uacute;n resultado coincide con tu b&uacute;squeda</p>}
+          </div>
         </div>
       </div>
-      {/* {loading
-        ? <p>Loading...</p>
-        : data && data.allMovies[0].results.map(movie => (
-          <div key={movie.title}>{movie.title}</div>
-        ))} */}
-    </>
+    </div>
   )
 }
 
