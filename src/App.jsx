bfc6700171c226ece6c0aaf82feb6c2776cb026f
davidnherran <davidnherran@gmail.com>
2022-02-18 02:03:28 -0500

    :construction: Add: Linter & Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-const App =()=> {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Exercise1 from './pages/Exercise1'
+
+const App = () => {
   return (
-    <div>  
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Exercise1 />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/Exercise1.jsx
@@ -0,0 +1,7 @@
+const Exercise1 = () => {
+  return (
+    <div>Exercise1</div>
+  )
+}
+
+export default Exercise1
